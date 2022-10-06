244fbe8de438fb8402d4c82d408675dd4f1c8d25
jyg <yonggyujeong10@gmail.com>
2022-10-06 22:43:22 +0900

    api gateway Global Filter, Logging Filter 추가

---
+++ src/main/java/com/example/gatewayservice/filter/GlobalFilter.java
@@ -0,0 +1,49 @@
+package com.example.gatewayservice.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+
+    public GlobalFilter() {
+        super(Config.class);
+    }
+
+    @Data
+    public static class Config{
+        // 설정 정보
+        String baseMessage;
+        boolean preLogger;
+        boolean postLogger;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        //Custom PreFilter
+        return (exchange, chain) ->{
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("GlobalFilter baseMessage: -> {}", config.getBaseMessage());
+
+            if(config.isPreLogger()){ //prefilter가 작동되어야 한다면
+                log.info("GlobalFilter Start: -> request id -> {}", request.getId());
+            }
+
+            // Custom PostFilter
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> { // Mono: spring 5 비동기환경에서 단일값 전달하기 위한 클래스
+                if(config.isPreLogger()){
+                    log.info("Global Filter end: response code: -> {}", response.getStatusCode());
+                }
+            }));
+        };
+    }
+}
